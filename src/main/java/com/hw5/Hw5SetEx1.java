348f5a8a1efa6f21af4df70e3541ead540f581d1
Aleksandr Kochkurkin <mailto:89636617290@mail.ru>
2019-09-06 00:13:16 +0200

    add hw5_part1

---
+++ src/main/java/com/hw5/Hw5SetEx1.java
@@ -0,0 +1,23 @@
+package com.hw5;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Hw5SetEx1 {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("Barsik1");
+        set.add("Barsik2");
+
+        List<String> list = new ArrayList<>();
+        list.add("barsik3");
+        list.add("barsik4");
+        set.addAll(list);
+        System.out.println(set);
+        //      set.removeAll(list);
+        //     System.out.println(set);
+        set.retainAll(list);
+    }
+}
